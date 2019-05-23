c7241b1dd5fd70c220c5144d21509e5396cc5c3b
League Student <league@wtsim17.attlocal.net>
2019-05-23 16:59:55 -0700

    Updated S Block

--- src/default_package/SBlock.java
+++ src/default_package/SBlock.java
@@ -1,5 +1,16 @@
 package default_package;
 
-public class SBlock {
-
+public class SBlock extends GameObject{
+	public SBlock(Grid grid) {
+		super(grid);
+		xPos[0] = 3;
+		yPos[0] = 1;
+		xPos[1] = 4;
+		yPos[1] = 0;
+		xPos[2] = 4;
+		yPos[2] = 4;
+		xPos[3] = 5;
+		yPos[3] = 0;
+		//still working on S Block
+	}
 }
